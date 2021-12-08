67ae07ea5df7506454a9f79e33aa352fc1ac6f13
ViniciusCHR <vinicius.unicap@gmail.com>
2021-12-08 07:26:20 -0300

    rotas de entrar e cadastrar criadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
   import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroComponent } from './cadastro/cadastro.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full' },
-  {path: 'login', component: LoginComponent },
-  {path: 'cadastro', component: LoginComponent },
+  {path: 'login', component: LoginComponent},
+  {path: 'cadastro', component: CadastroComponent}
 ];
 
 
